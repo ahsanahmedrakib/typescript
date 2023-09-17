324f2b218d10f972ba9f5ddbcfb1c58eb49eaf7c
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2023-09-17 10:12:01 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var userName1 = "Ahsan";
+var userName2 = "Ahmed";
+var userName3 = "Rakib";
+console.log(userName1 + " " + userName2 + " " + userName3);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let userName1 = "Ahsan";
+let userName2 = "Ahmed";
+let userName3 = "Rakib";
+console.log(userName1 + " " + userName2 + " " + userName3);
