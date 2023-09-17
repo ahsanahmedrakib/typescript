78b774ff2205c8855e8ad8094328e1c1a50b442d
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2023-09-17 11:24:36 +0600

    Built in types

---
+++ builtInType.js
@@ -0,0 +1,13 @@
+// number, string, boolean, null, undefined, void
+// array, class, interface
+var userId;
+var userName;
+var isActive;
+userId = 1;
+userName = "John";
+isActive = true;
+console.log(userId, userName, isActive);
+function display() {
+    console.log("I am display function");
+}
+display();
---
+++ builtInType.ts
@@ -0,0 +1,18 @@
+// number, string, boolean, null, undefined, void
+// array, class, interface
+
+let userId: number;
+let userName: string;
+let isActive: boolean;
+
+userId = 1;
+userName = "John";
+isActive = true;
+
+console.log(userId, userName, isActive);
+
+function display():void {
+    console.log("I am display function");
+}
+
+display();
