c511dfbfd63145673c087693f005c0e2dfb82a8f
Ahsan Ahmed Rakib <ahsan.rakib@technonext.com>
2023-09-18 13:58:14 +0600

    Function signature

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ userName = "Ahsan Ahmed Rakib";
 function addNumbers(n1, n2) {
     console.log(n1 + n2);
 }
-addNumbers(1, 2);
+// addNumbers(1, 2);
 // console.log(userName);
+// function signature
+var userInfo1;
+var userInfo2;
+var userInfo3;
+userInfo1 = function () {
+    console.log("Rakib");
+};
+userInfo2 = function (name) {
+    console.log(name);
+};
+userInfo3 = function (name, age) {
+    return name + " " + age;
+};
+userInfo1();
+userInfo2("Ahsan");
+console.log(userInfo3("Ahmed", 20));
--- index.ts
+++ index.ts
@@ -4,5 +4,25 @@ userName = "Ahsan Ahmed Rakib";
 function addNumbers(n1: number, n2: number) {
   console.log(n1 + n2);
 }
-addNumbers(1, 2);
+// addNumbers(1, 2);
 // console.log(userName);
+
+// function signature
+
+let userInfo1: () => void;
+let userInfo2: (name: string) => void;
+let userInfo3: (name: string, age: number) => string;
+
+userInfo1 = () => {
+  console.log("Rakib");
+};
+userInfo2 = (name: string) => {
+  console.log(name);
+};
+userInfo3 = (name: string, age: number) => {
+  return name + " " + age;
+};
+
+userInfo1();
+userInfo2("Ahsan");
+console.log(userInfo3("Ahmed", 20));
